c323fc1dc3034c3ef3cb14c4ba22d9e2b1d66de5
Jakub Wilczek <jacob_0909@icloud.com>
2026-02-07 00:38:34 +0100

    First commit second device and feet_inches new code

--- feet_inches.py
+++ feet_inches.py
@@ -1,1 +1,15 @@
 feet_inches = input("Enter feet and inches  ")
+
+
+def convert(feet_inches):
+    f,i = feet_inches.split( )
+    return f,i
+
+
+
+def printing(arg1,arg2):
+    print(arg1,arg2)
+
+
+f,i = convert(feet_inches)
+printing(f,i)
