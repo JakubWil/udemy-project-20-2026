ba89986d2e19dc2ef3b53fb292a76fe6e4449089
Jakub Wilczek <jacob_0909@icloud.com>
2026-02-07 01:21:38 +0100

    Add docstring to main.py

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 def get_todos(filepath="todos.txt"):
+
+    """
+    Odczytuje plik i zapisuje rezultat do tablicy
+
+    Argumenty:
+        fielpath: (string): plik tekstowy do odczutu
+
+    
+    Zwraca:
+        tablice rezultatu odczytu pliku
+
+    """
+
     with open(filepath, "r") as file:
         todos = file.readlines()
     return todos
 
 
 def write_todos(todos, filepath="todos.txt"):
-     with open(filepath, "w") as file:
+     
+    """
+    Zapisuje do pliku tekstowego tablice 
+
+    Argumenty:
+        todos: tablica z zadaniami to-do
+        fielpath: (string): plik tekstowy do odczutu
+
+    
+    Zwraca:
+        Nic 
+
+    """
+    with open(filepath, "w") as file:
         file.writelines(todos)
 
 
